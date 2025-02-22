c6f340a0c6907138e254208d936763dbbbabdcac
Dev Palwar <devpalwar06@gmail.com>
2025-02-23 03:55:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const admin = require("firebase-admin");
 
 config();
 
+console.log(process.env.TYPE);
+
 const app = express();
 const PORT = 3000;
 
@@ -34,17 +36,9 @@ if (bot) {
 }
 
 const serviceAccount = {
-  "type": "service_account",
-  "project_id": "telegram-bot-chunnilaal-v2",
-  "private_key_id": "cec0acfd62b740d72b60685062f52b3ca2d08e35",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQDWWDR3rGNF/I/e\n08XiPqHnGd0BYXSbxmF0E9rjWxQckfdDG+suem4xWoZpHiZX/MGwbUMub8vqJkKH\nWK9s731GFl6zFIJSOS/EYwaCFXVzWMB/2pp/4L8DRu8hvqhZNvqGKheAGEIfi3be\na4LkJQIjsh9m/a+Gm2bvSIfbw8ZukC2egdSLwkdOW5IqC7qZootQnbuTKDiuOXS2\n6ukQs30fXyEIfixkMhszaUizJ8Jp2OKwDDcTDQgMfcQlmt2rpxJ2PetuzZfY5xQk\nAg/1GbUcb3kkZ+3WCVZFCWjVSEv/MPSuSHWeVxDe+M1WaRBsqtwnMuQqFBqnyhin\n+frBcxTNAgMBAAECggEACzbuYymK6yU++Mi0lIpe3wV10FCYm4g1CUVQVpoUErea\nC9yTgOkecqjFb2KzXQyoLKfW0+s9AclSgfGEV4CDRINp8oLa8QbgG9gWMIBYwWTO\nHj0M3YPKiAHHhAjilte0eAhj64uOtXGkUe2m7vK3FTDeCZMx18Ax50ibSnFd5I3A\nMV3UwiKJPg6h62dgW4ziwRvQ4LyJWEw79zPkfBcINL3LX1f//7NwD4XPfIESGeSV\nY9qQcRykoRzqzYNraJY9XhDo/SpAOgvkQ+CLfuftlToC627725MZfxwgrv7DpskR\nBu1XRDo2d571rojX+U8K1w7EcG8KH6jHoKUjj+XgmQKBgQD7RTKUcfU764MyEXZJ\ndXBBdgGa9+9jiDqsMeZiRiSN3YryG2P9jfmdIxH1v8bxhOG6c85oKfFqS0zhkrwH\nFxLuacVoTMSVeKW7zaTnqvrY7BDBDFDYyuehYeTsLv7rEGK2CRz7NjUIywpzGA5o\nVksaiZFJEoACDONrXghdnPVdNQKBgQDaYRKErxjmryZlW1aKcAnGjcYuHaB3Q7Qk\nTI+iNgwvMEJPOxfhWbmdVI7eqHQWvzlSRenIPYW56CwIQ1q0HL1bJ8s/x1AJcLCp\n2e8IPVJ7RRMcDes5HKelYT64WvJu7xaHXRo+CJVNndnC5cAfvGiXoZSHPIYf1Qli\nauRj0fqEOQKBgQCzMsd+zML7p9vaElAM1Z9oYBOHBLmKf3ZwpenLPoEFV/Gm9tfZ\n1a4mXgOKAuqMBpvBkKOs7/THPMqouTA10kPjGjX6S32LqA5yoJexzOO3XqwYMYNb\nCWImWx2bP2z4EaiEcH1lv2QVUFC0gnyL9rb7Rw8BURtP2wc55EcNFEp83QKBgQCs\nAkwY0OaXRZzDt0caMhexGpjgWBu99UkeUmPMjC/at3NzfwNXUFpAz4rdXOhbxnF7\nGXCEddkwTuzn8U2vkvefpl+UtASZ+vZcQznhTpP6XrUnotsFmEmAnXFOuuw8bvxa\n0P3LoO0hPtV4i4liNgBYd0BLqWOoDSCgMbwF2C4wGQKBgQDTr1TodlEh9dlwMPpN\n1dO8gAODBQgwv1RZrUDFUA6654+6+A8vyXhosLN6Ug3Cpv5wRPYpJ5N0Qz8dDi34\naVA5KATNatWfAD0ctPR8MCJnxjnvI/dDYuboK44llbAHMXwVP0BnOz4l2Y91XNiX\nbMydPmubnaV7DkUD3vGm2WRw8A==\n-----END PRIVATE KEY-----\n",
-  "client_email": "firebase-adminsdk-fbsvc@telegram-bot-chunnilaal-v2.iam.gserviceaccount.com",
-  "client_id": "104231239139709751700",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40telegram-bot-chunnilaal-v2.iam.gserviceaccount.com",
-  "universe_domain": "googleapis.com"
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
 };
 
 admin.initializeApp({
